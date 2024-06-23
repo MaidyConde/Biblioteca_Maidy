ad6d44d1e0c6cf626623029073183f422f1e0ac5
Maidy Conde <maidyladino04@gmail.com>
2024-06-23 18:01:07 -0500

    registra libro

--- Front-end/js/Libro.js
+++ Front-end/js/Libro.js
@@ -20,7 +20,7 @@ function listarLibro() {
                     <td class="text-center align-middle">${result[i]["numeroEjemplaresDisponibles"]}</td>
                     <td class="text-center align-middle">${result[i]["numeroEjemplaresOcupados"]}</td>
                     <td class="text-center align-middle">
-                        <i class="fas fa-edit editar"  onclick="registrarLibroBandera=false;" data-id="${result[i]["idLibro"]}"></i>
+                        <i class="fas fa-edit editar"  onclick="editar;" data-id="${result[i]["idLibro"]}"></i>
                         <i class="fas fa-trash-alt eliminar" data-id="${result[i]["idLibro"]}"></i>
                     </td>
                 `;
@@ -54,7 +54,7 @@ function RegistrarLibro() {
         // Mostrar una alerta indicando que todos los campos son obligatorios
         Swal.fire({
             title: "¡Error!",
-            text: "¡Llene todos los campos correctamente!1",
+            text: "¡Llene todos los campos correctamente!",
             icon: "error"
         });
         return; // Salir de la función si algún campo está vacío
@@ -194,13 +194,12 @@ function validarisbn(cuadroNumero) {
     return valido;
 }
 
-// Función apellidocliente
-function validarCamposapellidocliente() {
-    var apellidoCliente = document.getElementById("apellidoCliente");
-    return validarapellidocliente(apellidoCliente);
+function validarCamposgenero() {
+    var Genero = document.getElementById("Genero");
+    return validargenero(Genero);
 }
 
-function validarapellidocliente(cuadroNumero) {
+function validargenero(cuadroNumero) {
     var valor = cuadroNumero.value;
     var valido = true;
 
@@ -217,13 +216,36 @@ function validarapellidocliente(cuadroNumero) {
     return valido;
 }
 
-// Función Telefono
-function validarCampostelefono() {
-    var Telefono = document.getElementById("Telefono");
-    return validartelefono(Telefono);
+// Función apellidocliente
+function validarCamposnumeroEjemplaresDisponibles() {
+    var numeroEjemplaresDisponibles = document.getElementById("numeroEjemplaresDisponibles");
+    return validarnumeroEjemplaresDisponibles(numeroEjemplaresDisponibles);
 }
 
-function validartelefono(cuadroNumero) {
+function validarnumeroEjemplaresDisponibles(cuadroNumero) {
+    var valor = cuadroNumero.value;
+    var valido = true;
+
+    if (valor.length < 1 || valor.length > 45) {
+        valido = false;
+    }
+
+    if (valido) {
+        cuadroNumero.className = "form-control is-valid";
+    } else {
+        cuadroNumero.className = "form-control is-invalid";
+    }
+
+    return valido;
+}
+
+
+function validarCamposnumeroEjemplaresOcupados() {
+    var numeroEjemplaresOcupados = document.getElementById("numeroEjemplaresOcupados");
+    return validarnumeroEjemplaresOcupados(numeroEjemplaresOcupados);
+}
+
+function validarnumeroEjemplaresOcupados(cuadroNumero) {
     var valor = cuadroNumero.value;
     var valido = true;
 
@@ -240,101 +262,12 @@ function validartelefono(cuadroNumero) {
     return valido;
 }
 
-// Función direccion
-function validarCamposdireccion() {
-    var Direccion = document.getElementById("Direccion");
-    return validardireccion(Direccion);
-}
-
-function validardireccion(cuadroNumero) {
-    var valor = cuadroNumero.value;
-    var valido = true;
-
-    if (valor.length < 1 || valor.length > 45) {
-        valido = false;
-    }
-
-    if (valido) {
-        cuadroNumero.className = "form-control is-valid";
-    } else {
-        cuadroNumero.className = "form-control is-invalid";
-    }
-
-    return valido;
-}
-
-function validarCamposciudad() {
-    var Ciudad = document.getElementById("Ciudad");
-    return validarciudad(Ciudad);
-}
-
-function validarciudad(cuadroNumero) {
-    var valor = cuadroNumero.value;
-    var valido = true;
-
-    if (valor.length < 1 || valor.length > 45) {
-        valido = false;
-    }
-
-    if (valido) {
-        cuadroNumero.className = "form-control is-valid";
-    } else {
-        cuadroNumero.className = "form-control is-invalid";
-    }
-
-    return valido;
-}
-
-function validarCamposcorreo() {
-    var Correo = document.getElementById("Correo");
-    return validarcorreo(Correo);
-}
-
-function validarcorreo(cuadroNumero) {
-    var valor = cuadroNumero.value;
-    var valido = true;
-
-    if (valor.length < 1 || valor.length > 45) {
-        valido = false;
-    }
-
-    if (valido) {
-        cuadroNumero.className = "form-control is-valid";
-    } else {
-        cuadroNumero.className = "form-control is-invalid";
-    }
-
-    return valido;
-}
-// Función Estado
-function validarCamposEstado() {
-    var Estado = document.getElementById("Estado");
-    return validarestado(Estado);
-}
-
-function validarestado(cuadroNumero) {
-    var valor = cuadroNumero.value;
-    var valido = true;
-
-    if (valor.length < 1 || valor.length > 2) {
-        valido = false;
-    }
-
-    if (valido) {
-        cuadroNumero.className = "form-select is-valid";
-    } else {
-        cuadroNumero.className = "form-select is-invalid";
-    }
-
-    return valido;
-}
-
-function AplicarFiltros(ciudad) {
-    if (ciudad=== '') {
+function AplicarFiltros(filtro) {
+    if (filtro=== '') {
         listarCliente(); // Mostrar todos los médicos si estado es vacío
     }else{
         $.ajax({
-            url: "http://localhost:8080/api/v1/Clientes/busquedafiltrociudad/" + ciudad,
+            url: "http://localhost:8080/api/v1/Libro/busquedafiltro/" + filtro,
             type: "GET",
             success: function (result) {
                 var cuerpoTabla = document.getElementById("cuerpoTabla");
@@ -343,20 +276,16 @@ function AplicarFiltros(ciudad) {
                 for (var i = 0; i < result.length; i++) {
                     var trRegistro = document.createElement("tr");
                     trRegistro.innerHTML = `
-                        <td>${result[i]["idClientes"]}</td>
-                        <td class="text-center align-middle">${result[i]["tipoIdentificacion"]}</td>
-                        <td class="text-center align-middle">${result[i]["Identificacion"]}</td>
-                        <td class="text-center align-middle">${result[i]["nombreCliente"]}</td>
-                        <td class="text-center align-middle">${result[i]["apellidoCliente"]}</td>
-                        <td class="text-center align-middle">${result[i]["Telefono"]}</td>
-                        <td class="text-center align-middle">${result[i]["Direccion"]}</td>
-                        <td class="text-center align-middle">${result[i]["Ciudad"]}</td>
-                        <td class="text-center align-middle">${result[i]["Correo"]}</td>
-                        <td class="text-center align-middle">${result[i]["Estado"]}</td>
+                        <td>${result[i]["idLibro"]}</td>
+                        <td class="text-center align-middle">${result[i]["Titulo"]}</td>
+                        <td class="text-center align-middle">${result[i]["Autor"]}</td>
+                        <td class="text-center align-middle">${result[i]["ISBN"]}</td>
+                        <td class="text-center align-middle">${result[i]["Genero"]}</td>
+                        <td class="text-center align-middle">${result[i]["numeroEjemplaresDisponibles"]}</td>
+                        <td class="text-center align-middle">${result[i]["numeroEjemplaresOcupados"]}</td>
                         <td class="text-center align-middle">
-                            <i class="fas fa-edit editar"  onclick="registrarClienteBandera=false;" data-id="${result[i]["idClientes"]}"></i>
-                            <i class="fas fa-user-slash cambiarEstado" onclick="cambiarEstado(${result[i]["idClientes"]})" data-id="${result[i]["idClientes"]}"></i>
-                            <i class="fas fa-trash-alt eliminar" data-id="${result[i]["idClientes"]}"></i>
+                            <i class="fas fa-edit editar"  onclick="editar;" data-id="${result[i]["idLibro"]}"></i>
+                            <i class="fas fa-trash-alt eliminar" data-id="${result[i]["idLibro"]}"></i>
                         </td>
                     `;
                     cuerpoTabla.appendChild(trRegistro);
@@ -370,100 +299,20 @@ function AplicarFiltros(ciudad) {
     
 }
 
-function AplicarFiltros(estado) {
-    if (estado === '') {
-        listarCliente(); // Mostrar todos los médicos si estado es vacío
-    } else if (estado === 'A','I') {
-        // Mostrar solo los médicos habilitados si estado es 'A'
-        $.ajax({
-            url: "http://localhost:8080/api/v1/Clientes/busquedafiltroestado/" + estado,
-            type: "GET",
-            success: function (result) {
-                var cuerpoTabla = document.getElementById("cuerpoTabla");
-                cuerpoTabla.innerHTML = "";
-
-                for (var i = 0; i < result.length; i++) {
-                    var trRegistro = document.createElement("tr");
-                    trRegistro.innerHTML = `
-                        <td>${result[i]["idClientes"]}</td>
-                        <td class="text-center align-middle">${result[i]["tipoIdentificacion"]}</td>
-                        <td class="text-center align-middle">${result[i]["identificacion"]}</td>
-                        <td class="text-center align-middle">${result[i]["nombreCliente"]}</td>
-                        <td class="text-center align-middle">${result[i]["apellidoCliente"]}</td>
-                        <td class="text-center align-middle">${result[i]["telefono"]}</td>
-                        <td class="text-center align-middle">${result[i]["direccion"]}</td>
-                        <td class="text-center align-middle">${result[i]["ciudad"]}</td>
-                        <td class="text-center align-middle">${result[i]["correo"]}</td>
-                        <td class="text-center align-middle">${result[i]["estado"]}</td>
-                        <td class="text-center align-middle">
-                            <i class="fas fa-edit editar"  onclick="registrarClienteBandera=false;" data-id="${result[i]["idClientes"]}"></i>
-                            <i class="fas fa-user-slash cambiarEstado" onclick="cambiarEstado(${result[i]["idClientes"]})" data-id="${result[i]["idClientes"]}"></i>
-                            <i class="fas fa-trash-alt eliminar" data-id="${result[i]["idClientes"]}"></i>
-                        </td>
-                    `;
-                    cuerpoTabla.appendChild(trRegistro);
-                }
-            },
-            error: function (error) {
-                alert("Error en la petición: " + error);
-            }
-        });
-    } else {
-        // Mostrar solo los médicos deshabilitados si no es vacío ni 'A'
-        $.ajax({
-            url: "http://localhost:8080/api/v1/Cliente/busquedafiltroestado/" + estado,
-            type: "GET",
-            success: function (result) {
-                var cuerpoTabla = document.getElementById("cuerpoTabla");
-                cuerpoTabla.innerHTML = "";
-
-                for (var i = 0; i < result.length; i++) {
-                    var trRegistro = document.createElement("tr");
-                    trRegistro.innerHTML = `
-                        <td>${result[i]["idClientes"]}</td>
-                        <td class="text-center align-middle">${result[i]["tipoIdentificacion"]}</td>
-                        <td class="text-center align-middle">${result[i]["Identificacion"]}</td>
-                        <td class="text-center align-middle">${result[i]["nombreCliente"]}</td>
-                        <td class="text-center align-middle">${result[i]["apellidoCliente"]}</td>
-                        <td class="text-center align-middle">${result[i]["Telefono"]}</td>
-                        <td class="text-center align-middle">${result[i]["Direccion"]}</td>
-                        <td class="text-center align-middle">${result[i]["Ciudad"]}</td>
-                        <td class="text-center align-middle">${result[i]["Estado"]}</td>
-                        <td class="text-center align-middle">
-                            <i class="fas fa-edit editar"  onclick="registrarMedicoBandera=false;" data-id="${result[i]["idClientes"]}"></i>
-                            <i class="fas fa-user-slash cambiarEstado" onclick="cambiarEstado(${result[i]["idClientes"]})" data-id="${result[i]["idClientes"]}"></i>
-                            <i class="fas fa-trash-alt eliminar" data-id="${result[i]["idClientes"]}"></i>
-                        </td>
-                    `;
-                    cuerpoTabla.appendChild(trRegistro);
-                }
-            },
-            error: function (error) {
-                alert("Error en la petición: " + error);
-            }
-        });
-    }
-}
 // Función para limpiar campos del formulario
 function limpiar() {
-    document.getElementById("tipoIdentificacion").value = "";
-    document.getElementById("tipoIdentificacion").className = "form-control";
-    document.getElementById("Identificacion").value = "";
-    document.getElementById("Identificacion").className = "form-control";
-    document.getElementById("nombreCliente").value = "";
-    document.getElementById("nombreCliente").className = "form-control";
-    document.getElementById("apellidoCliente").value = "";
-    document.getElementById("apellidoCliente").className = "form-control";
-    document.getElementById("Telefono").value = "";
-    document.getElementById("Telefono").className = "form-control";
-    document.getElementById("Direccion").value = "";
-    document.getElementById("Direccion").className = "form-control";
-    document.getElementById("Ciudad").value = "";
-    document.getElementById("Ciudad").className = "form-control";
-    document.getElementById("Correo").value = "";
-    document.getElementById("Correo").className = "form-control";
-    document.getElementById("Estado").value = "";
-    document.getElementById("Estado").className = "form-control";
+    document.getElementById("Titulo").value = "";
+    document.getElementById("Titulo").className = "form-control";
+    document.getElementById("Autor").value = "";
+    document.getElementById("Autor").className = "form-control";
+    document.getElementById("ISBN").value = "";
+    document.getElementById("ISBN").className = "form-control";
+    document.getElementById("Genero").value = "";
+    document.getElementById("Genero").className = "form-control";
+    document.getElementById("numeroEjemplaresDisponibles").value = "";
+    document.getElementById("numeroEjemplaresDisponibles").className = "form-control";
+    document.getElementById("numeroEjemplaresOcupados").value = "";
+    document.getElementById("numeroEjemplaresOcupados").className = "form-control";
 }
 
 var idLibro = "";
@@ -473,44 +322,22 @@ $(document).on("click", ".editar", function () {
     idLibro = $(this).data("id");
 
     $.ajax({
-        url: url + idClientes,
+        url: url + idLibro,
         type: "GET",
-        success: function (clientes) {
-            document.getElementById("tipoIdentificacion").value = clientes.tipoIdentificacion;
-            document.getElementById("Identificacion").value = clientes.Identificacion;
-            document.getElementById("nombreCliente").value = clientes.nombreCliente;
-            document.getElementById("apellidoCliente").value = clientes.apellidoCliente;
-            document.getElementById("Telefono").value = clientes.Telefono;
-            document.getElementById("Direccion").value = clientes.Direccion;
-            document.getElementById("Ciudad").value = clientes.Ciudad;
-            document.getElementById("Estado").value = clientes.Estado;
+        success: function (libro) {
+            document.getElementById("Titulo").value = libro.Titulo;
+            document.getElementById("Autor").value = libro.Autor;
+            document.getElementById("ISBN").value = libro.ISBN;
+            document.getElementById("Genero").value = libro.Genero;
+            document.getElementById("numeroEjemplaresDisponibles").value = libro.numeroEjemplaresDisponibles;
+            document.getElementById("numeroEjemplaresOcupados").value = libro.numeroEjemplaresOcupados;
             $('#exampleModal').modal('show');
         },
         error: function (error) {
-            alert("Error al obtener los datos del cliente: " + error.statusText);
+            alert("Error al obtener los datos del libro: " + error.statusText);
         }
     });
 });
-
-$(document).on("click", ".cambiarEstado", function () {
-    var idLibro = $(this).data("id");
-    $.ajax({
-        url: url + idLibro,
-        type: "DELETE",
-        success: function () {
-            Swal.fire({
-                position: "top-end",
-                icon: "success",
-                title: "Cambio de estado exitoso",
-                showConfirmButton: false,
-                timer: 1500
-            });
-            listarLibro(); // Actualiza la lista de pacientes en el front-end
-        }
-    });
-});
-
-
 
 $(document).on("click", ".eliminar", function () {
     // Obtener el ID del médico desde el atributo data del elemento clicado
